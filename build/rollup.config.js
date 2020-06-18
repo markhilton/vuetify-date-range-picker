2470d967481b4e22f889473ed1803bdddb9637a6
Mark Hilton <nerd305@gmail.com>
2020-06-18 14:28:46 -0400

    bug fixes in npm publishing

---
+++ build/rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs" // Convert CommonJS modules to ES6
+import vue from "rollup-plugin-vue" // Handle .vue SFC files
+import buble from "@rollup/plugin-buble" // Transpile/polyfill with reasonable browser support
+export default {
+  input: "src/wrapper.js", // Path relative to package.json
+  output: {
+    name: "VuetifyDateRangePicker",
+    exports: "named",
+  },
+  plugins: [
+    resolve(),
+    commonjs({
+      include: "node_modules/**",
+    }),
+    vue({
+      css: true, // Dynamically inject css as a <style> tag
+      compileTemplate: true, // Explicitly convert template to render function
+    }),
+    buble(), // Transpile to ES5
+  ],
+}
