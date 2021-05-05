de6712791a24e23238d51d2b001c458518725d26
Mark Hilton <nerd305@gmail.com>
2021-05-05 12:41:01 -0400

    change "compare to:" => "vs" to save space

--- src/components/DatePicker/DateSelector.vue
+++ src/components/DatePicker/DateSelector.vue
@@ -11,7 +11,7 @@
         {{ getFormattedDate(getDateStart) }} &mdash; {{ getFormattedDate(getDateUntil) }}
 
         <small v-if="config.compare" class="d-flex mt-n2">
-          Compare to: {{ getFormattedDate(getDateCompareStart) }} &mdash; {{ getFormattedDate(getDateCompareUntil) }}
+          vs {{ getFormattedDate(getDateCompareStart) }} &mdash; {{ getFormattedDate(getDateCompareUntil) }}
         </small>
       </v-col>
     </v-row>
